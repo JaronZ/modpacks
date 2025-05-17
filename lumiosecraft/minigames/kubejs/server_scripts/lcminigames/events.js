0f5f9fd499b95f5491bbf366d9d7154e788214b3
Jaron Zijlstra <support@jaronline.dev>
2025-05-17 13:58:42 +0200

    chore: remove lumiosecraft from this repo

--- lumiosecraft/minigames/kubejs/server_scripts/lcminigames/events.js
+++
@@ -1,5 +0,0 @@
-//priority: 0
-PlayerEvents.chat(event => {
-    if (!global.currentMinigame || !("chat" in global.currentMinigame)) return;
-    global.currentMinigame.chat(event);
-});
--- lumiosecraft/src/kubejs/server_scripts/pkgbadges/tags/block.js
+++
@@ -1,4 +0,0 @@
-ServerEvents.tags("block", event => {
-    event.remove("minecraft:mineable/pickaxe", "pkgbadges:league_cup");
-    event.add("minecraft:mineable/pickaxe", "pkgbadges:league_cup");
-});
